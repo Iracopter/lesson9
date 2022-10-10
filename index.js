ec965ba552976d5bdc06a0adc92134d5433c5ebc
Iracopter <114257500+Iracopter@users.noreply.github.com>
2022-10-10 21:48:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var a= prompt("Завдання 1. Введіть число a =","");
+var b= prompt("Завдання 1. Введіть число b =","");
+var suma = function add(x,y) {
+
+    return Number(x)+Number(y);
+    
+};
+    
+console.log(suma(a,b));
